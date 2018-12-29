1a0214a1bfbad8aaefc7167e3af0ed03f8ede0ee
David Velarde <davidvpe@gmail.com>
2018-12-29 09:54:18 +0100

    updated to https

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express')
+var fs = require('fs')
+var https = require('https')
+var app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/server.davidvpe.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/server.davidvpe.com/fullchain.pem')
+}, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
